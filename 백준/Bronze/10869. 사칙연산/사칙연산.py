0bbe0b2c72c3522d9f9d9afc06158fcc024a8f27
Sihyun Lee <arevolvera@gmail.com>
2024-01-12 14:45:57 +0900

    [Bronze V] Title: 사칙연산, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.py
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -1,14 +1,9 @@
-# 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
 import sys
 input = sys.stdin.readline
 
-temp = input().split()
-a = int(temp[0])
-b = int(temp[1])
-
+a, b = map(int, input().split())
 print(a+b)
 print(a-b)
 print(a*b)
 print(a//b)
 print(a%b)
-
