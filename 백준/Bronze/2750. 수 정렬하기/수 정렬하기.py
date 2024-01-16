c25c4367382c48e440773ccc6ff0cbaedaad5576
Sihyun Lee <moorow0729@hufs.ac.kr>
2024-01-16 18:19:12 +0900

    [Bronze II] Title: 수 정렬하기, Time: 180 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -4,12 +4,11 @@ array = []
 for i in range(0, N):
     number = int(input()) 
     array.append(number)
-    key = number
     j = i - 1
-    while j > -1 and array[j] > key :
+    while j > -1 and array[j] > number :
         array[j + 1] = array[j]
         j = j - 1
-    array[j + 1] = key
+    array[j + 1] = number
 
 for i in range(len(array)):
     print(array[i])
