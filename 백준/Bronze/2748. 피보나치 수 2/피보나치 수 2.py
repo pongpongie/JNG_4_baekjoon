6abfd842cc35c15fc8bdab7f09f29fb5122755a9
Sihyun Lee <moorow0729@hufs.ac.kr>
2024-01-29 14:12:47 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -2,16 +2,13 @@ import sys
 input = sys.stdin.readline
 
 def fibo2(num : int) -> int:
-    cnt = [0, 1, 1]
-    if num == 0:
-        return 0
-    if num == 1:
-        return 1
-    if num == 2:
-        return 1
-    else:
-        for i in range(3, num+1):
-            cnt.append(cnt[i-1] + cnt[i-2])
+    cnt = [0, 1]
+    for i in range(2, num+1):
+        cnt.append(cnt[i-1] + cnt[i-2])
     return cnt[num]
 
-print(fibo2(int(input())))
+print(fibo2(int(input()))) 
+
+
+
+
