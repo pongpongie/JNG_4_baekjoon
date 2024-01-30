9404f629f8b618e27b2b6ef5c4cfdc988e6ec6d8
Sihyun Lee <moorow0729@hufs.ac.kr>
2024-01-30 20:10:45 +0900

    [Gold V] Title: 동전, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -1,21 +1,17 @@
 import sys
-input = sys.stdin.readline
 
-cointypes = [0]
-t = int(input())
+T = int(sys.stdin.readline())
 
-for _ in range(t):
-    n = int(input())
-    cointypes = list(map(int, input().split()))
-    m = int(input())
-    dp = [[0] * (m+1) for _ in range(n+1)]
+for _ in range(T):
 
-    for i in range(1, n+1):
-        dp[i][0] = 1
+    N = int(sys.stdin.readline())
+    coins = list(map(int, sys.stdin.readline().split()))
+    M = int(sys.stdin.readline())
 
-    for i in range(1, n+1):
-        for j in range(1, m+1):
-            dp[i][j] = dp[i-1][j]
-            if j - cointypes[i-1] >= 0:
-                dp[i][j] += dp[i][j-cointypes[i-1]]
-    print(dp[n][m])
+    dp = [0] * (M+1)
+    dp[0] = 1
+    for coin in coins:
+        for i in range(1, M+1):
+            if i >= coin:
+                dp[i] += dp[i-coin]
+    print(dp[M])
