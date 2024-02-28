ff7e60fb3b6222dccf27b852a52d3c71753bcdba
Sihyun Lee <moorow0729@hufs.ac.kr>
2024-02-28 13:19:14 +0900

    [Silver V] Title: 나이순 정렬, Time: 264 ms, Memory: 61500 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,5 +1,3 @@
-# https://www.acmicpc.net/problem/10814
-
 import sys
 input = sys.stdin.readline
 
@@ -8,10 +6,8 @@ arr = []
 
 for i in range(n):
     arr.append(input().split())
-    arr[i][0] = int(arr[i][0])
-    arr[i].append(i)
 
-arr.sort(key = lambda x: (x[0], x[2]))
+arr.sort(key = lambda x: int(x[0]))
 
 for names in arr:
     print(names[0], names[1])
